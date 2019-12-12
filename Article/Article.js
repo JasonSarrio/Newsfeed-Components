6bd7fcfd32e898cff765cba369910ed794cd5220
JasonSarrio <jsarrio16@gmail.com>
2019-12-12 17:45:00 -0600

    Not Fininsh

--- Article/Article.js
+++ Article/Article.js
@@ -112,3 +112,41 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+const articles = document.querySelector(".articles");
+function createArticle(data) {
+  const article = document.createElement("div");
+  const articleTitle = document.createElement("h2");
+  const articleDate = document.createElement("p");
+  const articlePara1 = document.createElement("p");
+  const articlePara2 = document.createElement("p");
+  const articlePara3 = document.createElement("p");
+  const button = document.createElement("span");
+
+  article.appendChild(articleTitle);
+  article.appendChild(articleDate);
+  article.appendChild(articlePara1);
+  article.appendChild(articlePara2);
+  article.appendChild(articlePara3);
+  article.appendChild(button);
+
+  article.classList.add("article");
+  articleDate.classList.add("date");
+  button.classList.add("expandButton");
+
+  articleTitle.textContent = data.title;
+  articleDate.textContent = data.date;
+  articlePara1.textContent = data.firstParagraph;
+  articlePara2.textContent = data.secondParagraph;
+  articlePara3.textContent = data.thirdParagraph;
+
+  article.addEventListener("click", e => {
+    console.log("button clicked", e.target);
+    article.classList.toggle("article-open");
+  });
+
+  return article;
+}
+
+data.forEach(data => {
+  articles.appendChild(createArticle(data));
+});
